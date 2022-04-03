ee464c654bb4b3ff246961e979841a55fe8c069d
Ömer Faruk ZORLU <79523683+omerfarukzorlu@users.noreply.github.com>
2022-04-03 15:43:10 +0300

    Update start.sh

--- environment/openshift/scripts/node/start.sh
+++ environment/openshift/scripts/node/start.sh
@@ -15,12 +15,12 @@ echo "${NODE_IP_LIST}"
 
 
 
-[[ ${NODE_NAME} =~ [^0-9]*([0-9]*) ]]
-NODENUM=${BASH_REMATCH[1]}
-echo "Setting Up Indy Node Number $NODENUM"
-exec su - indy -c "init_indy_node ${NODE_NAME} ${NODE_PORT} ${CLIENT_PORT}"  # set up /etc/indy/indy.env 
-exec su - indy -c "generate_indy_pool_transactions --nodes 4 --clients 4 --nodeNum $NODENUM --ips ${NODE_IP_LIST}"
-exec systemctl start indy-node
-exec systemctl enable indy-node
-exec systemctl status indy-node.service
+#[[ ${NODE_NAME} =~ [^0-9]*([0-9]*) ]]
+#NODENUM=${BASH_REMATCH[1]}
+#echo "Setting Up Indy Node Number $NODENUM"
+#exec su - indy -c "init_indy_node ${NODE_NAME} ${NODE_PORT} ${CLIENT_PORT}"  # set up /etc/indy/indy.env 
+#exec su - indy -c "generate_indy_pool_transactions --nodes 4 --clients 4 --nodeNum $NODENUM --ips ${NODE_IP_LIST}"
+#exec systemctl start indy-node
+#exec systemctl enable indy-node
+#exec systemctl status indy-node.service
 
